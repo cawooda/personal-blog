641875549e9b4c16d70c980b82cefdbc332687c6
Andrew Cawood <cawooda@gmail.com>
2024-03-08 18:59:12 +1100

    genesis

---
+++ Assets/images/scripts/script.js
@@ -0,0 +1,8 @@
+lightDarkToggle = document.querySelector('#light-dark');
+
+function switchTheme() {
+    console.log("light Dark Selected");
+}
+
+lightDarkToggle.addEventListener('click',switchTheme);
+
