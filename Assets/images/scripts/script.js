41879f3564d752a60bfa770e2e046a0334a89032
Andrew Cawood <cawooda@gmail.com>
2024-03-09 15:26:39 +1100

    basic setup of theme switching working

--- Assets/images/scripts/script.js
+++
@@ -1,8 +0,0 @@
-lightDarkToggle = document.querySelector('#light-dark');
-
-function switchTheme() {
-    console.log("light Dark Selected");
-}
-
-lightDarkToggle.addEventListener('click',switchTheme);
-
---
+++ Assets/scripts/script.js
@@ -0,0 +1,44 @@
+//Theme Switching-->
+const Theme = { LIGHT:'light', DARK: 'dark'};
+const HTML_ELEMENT = document.documentElement;
+const STORAGE_KEY = 'theme';
+const cachedTheme = localStorage.getItem(STORAGE_KEY);
+const themeToggle = document.getElementById('theme-toggle');
+const MOON = "🌔"
+const SUN = "🌻"
+//checkes if a cached theme was found. if not it defaults to light
+if (cachedTheme) {
+    if (cachedTheme === Theme.DARK) {
+            HTML_ELEMENT.classList.add(Theme.DARK);
+            themeToggle.textContent = MOON;
+        } else {
+            HTML_ELEMENT.classList.add(Theme.DARK);
+            themeToggle.textContent = SUN;
+        }
+} else {
+    HTML_ELEMENT.classList.add(Theme.LIGHT);//defailts to light
+    themeToggle.textContent = SUN;
+}
+//Switches the theme
+function switchTheme() {
+    if (HTML_ELEMENT.classList.contains(Theme.LIGHT)) {
+        console.log("classlist contains Theme.LIGHT");
+        console.log(`HTML Classlist Before: ${HTML_ELEMENT.classList}`);
+        HTML_ELEMENT.classList.replace(Theme.LIGHT,Theme.DARK);
+        localStorage.setItem(STORAGE_KEY,Theme.DARK);
+        themeToggle.textContent = MOON;
+        console.log(`HTML Classlist AFTER: ${HTML_ELEMENT.classList}`);
+        console.log(`cached theme ${cachedTheme}`);
+    } else {
+        console.log(`HTML Classlist Before: ${HTML_ELEMENT.classList}`);
+        HTML_ELEMENT.classList.replace(Theme.DARK,Theme.LIGHT);
+        localStorage.setItem(STORAGE_KEY,Theme.LIGHT);
+        themeToggle.textContent = SUN;
+        console.log(`HTML Classlist AFTER: ${HTML_ELEMENT.classList}`);
+        console.log(`cached theme ${cachedTheme}`);
+    }
+}
+
+//EVENT LISTENERS
+themeToggle.addEventListener('click',switchTheme);
+
