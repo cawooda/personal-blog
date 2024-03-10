6e8e7e12ef82d371f23bddd94cf6a60e43581b8a
Andrew Cawood <cawooda@gmail.com>
2024-03-10 22:58:03 +1100

    working

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -7,6 +7,17 @@ const themeToggle = document.getElementById('theme-toggle');
 const MOON = "🌔"
 const SUN = "🌻"
 //checkes if a cached theme was found. if not it defaults to light
+
+//App Action
+const BLOG_LIST = 'blogList';
+
+const submitButton = document.getElementById('submitBlogPost');
+
+
+
+
+
+function getCachedTheme() {
 if (cachedTheme) {
     if (cachedTheme === Theme.DARK) {
             HTML_ELEMENT.classList.add(Theme.DARK);
@@ -19,6 +30,57 @@ if (cachedTheme) {
     HTML_ELEMENT.classList.add(Theme.LIGHT);//defailts to light
     themeToggle.textContent = SUN;
 }
+}
+
+function getPosts() {
+    const postsSection = document.getElementById('posts-section');
+    
+    const blogList = JSON.parse(localStorage.getItem(BLOG_LIST));
+    
+    
+    
+    if (blogList) {
+        let index = 0;
+        for (const post of blogList) {
+
+            console.log(post);
+            let date = new Date(parseInt(post.date));
+            console.log(date);
+            
+
+            let postElement = document.createElement('article');
+            postElement.setAttribute('id',`post-${index}`);
+
+            let title = document.createElement('h3');
+            title.textContent = post.title;
+            postElement.appendChild(title);
+            
+            const dateEl = document.createElement('p');
+            dateEl.className = 'blog-date';
+            console.log(dateEl);
+            dateEl.textContent = `Date: ${date.getDate()}/${date.getMonth()}/${date.getFullYear()}` ;
+            postElement.appendChild(dateEl);
+
+            let userName = document.createElement('p');
+            userName.className = 'user-name';
+            userName.textContent = `User: ${post.userName}`;
+            postElement.appendChild(userName);
+            
+
+            let content = document.createElement('p');
+            content.textContent = post.content;
+            postElement.appendChild(content);
+            
+            postsSection.appendChild(postElement);
+
+            index++;
+        }
+
+
+        postsSection.appendChild
+    } else return;
+}
+
 //Switches the theme
 function switchTheme() {
     if (HTML_ELEMENT.classList.contains(Theme.LIGHT)) {
@@ -39,6 +101,42 @@ function switchTheme() {
     }
 }
 
+//Event Handlers
+function submitBlogPostHandler(event) {
+    event.preventDefault();
+    const userNameInput = document.getElementById('username');
+    const blogTitleInput = document.getElementById('blog-title');
+    const blogContentInput = document.getElementById('blog-content');
+    let blogList = JSON.parse(localStorage.getItem(BLOG_LIST));
+    if (!blogList) blogList = [];
+    console.log(blogList);
+    
+    let blogPost = {}; 
+        blogPost.userName = userNameInput.value;
+        blogPost.date = Date.now();
+        blogPost.title = blogTitleInput.value;
+        blogPost.content = blogContentInput.value;
+    
+
+        blogList.push(blogPost);
+
+    console.log(blogList);
+    localStorage.setItem(BLOG_LIST,JSON.stringify(blogList));
+    
+    
+    window.location.href = "./posts.html";
+    
+
+}
+
+//Startup
+getCachedTheme();
+document.addEventListener("DOMContentLoaded", (event) => {
+    getPosts();
+  });
+
+
 //EVENT LISTENERS
+submitButton.addEventListener('click',submitBlogPostHandler);
 themeToggle.addEventListener('click',switchTheme);
 
